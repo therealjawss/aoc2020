b0939df9c887f245034036953ea71622d7ea4cac
jocelyn.englund <jocelyn.englund@coolcompany.se>
2020-12-06 07:31:40 +0100

    wep, fixing failed refactoring...

--- 2020/Days/Day6.cs
+++ 2020/Days/Day6.cs
@@ -10,98 +10,93 @@ using System.Threading.Tasks;
 
 namespace AOC2020.Days
 {
-	public class Day6 : Christmas
-	{
-		public override int Day => 6;
+    public class Day6 : Christmas
+    {
+        public override int Day => 6;
 
-		public override string Level1(string[] input)
-		{
-			var toAdd = new HashSet<char>();
-			long counter = 0;
-			for (int i = 0; i < input.Length; i++)
-			{
-				if (input[i].Length > 0)
-				{
-					for (int j = 0; j < input[i].Length; j++)
-					{
-						toAdd.Add(input[i][j]);
-					}
-				}
-				else
-				{
-					counter += toAdd.Count;
-					toAdd = new HashSet<char>();
-				}
+        public override string Level1(string[] input)
+        {
+            var toAdd = new HashSet<char>();
+            long counter = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i].Length > 0)
+                {
+                    for (int j = 0; j < input[i].Length; j++)
+                    {
+                        toAdd.Add(input[i][j]);
+                    }
+                }
+                else
+                {
+                    counter += toAdd.Count;
+                    toAdd = new HashSet<char>();
+                }
 
-			}
-			counter += toAdd.Count;
-			return counter.ToString();
-		}
+            }
+            counter += toAdd.Count;
+            return counter.ToString();
+        }
 
-		public override string Level2(string[] input)
-		{
-			List<char> answers = new List<char>();
-			long counter = 0;
-			int peopleCtr = 0;
-			for (int i = 0; i < input.Length; i++)
-			{
-				if (input[i].Length > 0)
-				{
-					peopleCtr++;
-					for (int j = 0; j < input[i].Length; j++)
-					{
-						answers.Add(input[i][j]);
-					}
-				}
-				else
-				{
-					counter += processGroupAnswers(answers, peopleCtr);
+        public override string Level2(string[] input)
+        {
+            List<char> answers = new List<char>();
+            var groupAnswers = new List<List<char>>();
+            long counter = 0;
+            int peopleCtr = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i].Length > 0)
+                {
+                    peopleCtr++;
+                    for (int j = 0; j < input[i].Length; j++)
+                    {
+                        answers.Add(input[i][j]);
+                    }
+                }
+                else
+                {
+                    counter += processGroupAnswers(answers, peopleCtr);
+                    answers = new List<char>();
+                    peopleCtr = 0;
 
-					answers = new List<char>();
-					peopleCtr = 0;
+                }
 
-				}
+            }
+            counter += processGroupAnswers(answers, peopleCtr);
+            return counter.ToString();
+        }
 
-			}
-			counter += processGroupAnswers(answers, peopleCtr);
+        private int processGroupAnswers(List<char> answers, int peopleCtr)
+        {
+            int counter = 0;
+            var result = answers.GroupBy(x => x);
+            foreach (var r in result)
+            {
+                if (r.Count() == peopleCtr)
+                {
+                    counter++;
+                }
+            }
+            return counter;
+        }
 
-			return counter.ToString();
-		}
+        public override string[] GetInput(string file = null, string pattern = null)
+        {
+            string buffer = base.ReadBuffer(file);
+            if (pattern == null)
+            {
+                Input = buffer.Split("\n").ToArray();
+            }
+            else
+            {
 
-		private long processGroupAnswers(List<char> answers, int peopleCtr)
-		{
-			var counter = 0;
-			var questions = answers.GroupBy(x => x);
+                Input = buffer.Split(pattern).ToArray();
+                //var r = new Regex(pattern);
+                // Input = Regex.Matches(buffer, pattern).Cast<Match>().Select(m => m.Value).ToArray();
+            }
+            return Input;
+        }
 
-			foreach (var question in questions)
-			{
-				if (question.Count() == peopleCtr)
-				{
-					counter++;
-				}
-			}
-
-			return peopleCtr;
-		}
-
-
-
-		public override string[] GetInput(string file = null, string pattern = null)
-		{
-			string buffer = base.ReadBuffer(file);
-			if (pattern == null)
-			{
-				Input = buffer.Split("\n").ToArray();
-			}
-			else
-			{
-
-				Input = buffer.Split(pattern).ToArray();
-				//var r = new Regex(pattern);
-				// Input = Regex.Matches(buffer, pattern).Cast<Match>().Select(m => m.Value).ToArray();
-			}
-			return Input;
-		}
-
-	}
+    }
 }
