514134904ef55c433ee608dc531e7c887ac1f92f
jocelyn.englund <jocelyn.englund@coolcompany.se>
2020-12-08 05:51:28 +0100

    prep for Day 8

--- AOCTest/Day8Test.cs
+++ AOCTest/Day8Test.cs
@@ -9,17 +9,8 @@ using Xunit;
 
 namespace AOCTest
 {
-	public class Day7Test
+	public class Day8Test
 	{
-		[Fact]
-		public void CanExtractRule()
-		{
-			List<Bag> bags = new List<Bag>();
-			var rulestring = "vibrant aqua bags contain 5 posh plum bags, 5 faded tomato bags, 5 shiny tomato bags, 1 mirrored orange bag.";
-			Bag rule = Day7.ParseRule(rulestring,bags);
-			rule.hasGold.Should().BeFalse();
-			rule.containedBags.Count.Should().Be(4);
-		}
 		[Fact]
 		public void CanParseInput()
 		{
