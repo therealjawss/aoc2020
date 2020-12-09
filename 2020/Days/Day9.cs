60978e1e9073a813e27fc8543e232195f33ae82b
jocelyn.englund <jocelyn.englund@coolcompany.se>
2020-12-09 08:23:42 +0100

    Day  8: slight improvement

--- 2020/Days/Day9.cs
+++ 2020/Days/Day9.cs
@@ -8,89 +8,84 @@ using System.Threading.Tasks;
 
 namespace AOC2020.Days
 {
-    public class Day9 : Christmas
-    {
-        public override int Day => 9;
+	public class Day9 : Christmas
+	{
+		public override int Day => 9;
 
-        public static void Run()
-        {
-            Console.WriteLine("hello");
-            var day = new Day9();
-            day.GetInput(file: "test.txt", pattern: "\r\n");
-            //  day.GetInput();
-            Console.WriteLine(day.Level1(day.Input));
-            //  day.PostL1Answer();
-            Console.WriteLine(day.Level2(day.Input));
-            //day.PostL2Answer();
-        }
-        List<ulong> realInput = new List<ulong>();
-        public override string Level1(string[] input)
-        {
-            var answer = findAnswer(input);
-            return answer.ToString();
+		public static void Run()
+		{
+			Console.WriteLine("hello");
+			var day = new Day9();
+			day.GetInput(file: "test.txt", pattern: "\r\n");
+			//  day.GetInput();
+			Console.WriteLine(day.Level1(day.Input));
+			//  day.PostL1Answer();
+			Console.WriteLine(day.Level2(day.Input));
+			//day.PostL2Answer();
+		}
+		List<long> RealInput = new List<long>();
+		public override string Level1(string[] input)
+		{
+			var answer = findAnswer(input);
+			return answer.ToString();
 
-        }
-        public ulong findAnswer(string[] input, int pream = 24)
-        {
-            realInput.Clear();
-            Array.ForEach(input, x => realInput.Add(Convert.ToUInt64(x)));
-            int start = 0;
-            int end = pream;
-            int j = 0;
-            int k = 0;
+		}
+		public long findAnswer(string[] input, int pream = 24)
+		{
+			RealInput.Clear();
+			Array.ForEach(input, x => RealInput.Add(Convert.ToInt64(x)));
+			int start = 0;
+			int end = pream;
+			int j = 0;
+			int k = 0;
 
-            for (int i = end + 1; i < realInput.Count; i++)
-            {
-                for (j = start; j <= end - 1; j++)
-                {
-                    for (k = start + 1; k <= end; k++)
-                    {
-                        if (k == j) continue;
-                        if (realInput[i] == realInput[j] + realInput[k])
-                        {
-                            j = end;
-                            k = end;
-                        };
-                    }
-                }
-                if (j == k)
-                {
-                    start++;
-                    end++;
-                    continue;
-                }
-                else return realInput[i];
-            }
-            return 0;
-        }
-
-        public override string Level2(string[] input)
-        {
-
-            var a = findAnswer(input);
-            for (int i = 0; i < realInput.Count - 2; i++)
-            {
-                var tempsum = realInput[i];
-                var s = realInput[i];
-                var l = realInput[i];
-                for (int j = i+1; j < realInput.Count - 1; j++)
-                {
-                    if (i == j) continue;
-                    tempsum += realInput[j];
-                    s = realInput[j] < s ? realInput[j] : s;
-                    l = realInput[j] > l ? realInput[j] : l;
-                    if (tempsum == a)
-                    {
-                        return (s + l).ToString();
-                    } else if (tempsum > a)
+			for (int i = end + 1; i < RealInput.Count; i++)
+			{
+				for (j = start; j <= end - 1; j++)
+				{
+					for (k = start + 1; k <= end; k++)
 					{
-                        break;
+						if (RealInput[i] == RealInput[j] + RealInput[k])
+						{
+							j = end;
+							k = end;
+						};
 					}
-                }
-            }
-            return "";
+				}
+				if (j == k)
+				{
+					start++;
+					end++;
+					continue;
+				}
+				else return RealInput[i];
+			}
+			return 0;
+		}
 
-        }
-    }
+		public override string Level2(string[] input)
+		{
+
+			var a = findAnswer(input);
+			for (int i = 0; i < RealInput.Count - 1; i++)
+			{
+				for (int j = i + 1; j < RealInput.Count; j++)
+				{
+					var currentRange = RealInput.Skip(i).Take(j - i + 1).ToList();
+					var sum = currentRange.Sum();
+					if (sum == a)
+					{
+						return (currentRange.Min() + currentRange.Max()).ToString();
+					}
+					else if (sum > a)
+					{
+						break;
+					}
+				}
+			}
+			return "none";
+
+		}
+	}
 
 }
