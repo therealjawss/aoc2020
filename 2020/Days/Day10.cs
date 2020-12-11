1514ee6bb814f1d93eb0230b89d9c6c41f1782b1
jocelyn.englund <jocelyn.englund@coolcompany.se>
2020-12-11 08:35:28 +0100

    Day11 part 1 dirty

--- 2020/Days/Day10.cs
+++ 2020/Days/Day10.cs
@@ -14,82 +14,84 @@ namespace AOC2020.Days
 
         public static void Run()
         {
-            Console.WriteLine("hello day 10");
             var day = new Day10();
+            Console.WriteLine($"hello day {day.Day}");
             //day.GetInput(file: "test.txt", pattern: "\r\n");
             day.GetInput();
-            //Console.WriteLine(day.Level1(day.Input));
+            Console.WriteLine(day.Level1(day.Input));
             //day.PostL1Answer();
             //    Thread.Sleep(60000);
             Console.WriteLine(day.Level2(day.Input));
             //day.PostL2Answer();
         }
         public override string Level1(string[] input)
-        {
-            var n = AsNumbers.OrderBy(x => x).ToArray();
-            int ones = 0;
-            int threes = 0;
-            int current = 0;
-            foreach (var item in n)
-            {
-                if (current + 1 == item)
-                {
-                    current += 1;
-                    ones++;
-                }
-                else if (current + 2 == item)
-                {
-                    current += 2;
+		{
+			var n = InputAsNumbers.OrderBy(x => x).ToList();
+			n.Add(0);
+			n = n.OrderBy(x => x).ToList();
+			n.Add(n[n.Count - 1] + 3);
 
-                }
-                else if (current + 3 == item)
-                {
-                    current += 3;
-                    threes++;
-                }
-            }
-            return (ones * ++threes).ToString();
+			int ones = 0;
+			int threes = 0;
+			int current = 0;
+
+			foreach (var item in n)
+			{
+				if (current + 1 == item)
+				{
+					current += 1;
+					ones++;
+				}
+				else if (current + 2 == item)
+				{
+					current += 2;
+				}
+				else if (current + 3 == item)
+				{
+					current += 3;
+					threes++;
+				}
+			}
+			return (ones * ++threes).ToString();
+		}
+
+		private static bool isThree(long x, List<long> n)
+		{
+			return n[n.IndexOf(x) + 1] -x == 3 |
+                n[n.IndexOf(x) + 2] - x == 3 |
+                n[n.IndexOf(x) + 3] - x == 3;
         }
-        private List<long> asNumbers = new();
-        public List<long> AsNumbers
+
+		private static bool isOne(long x, List<long> n)
+		{
+			return n[n.IndexOf(x) + 1] - x == 1;
+		}
+
+		private List<long> asNumbers = new();
+        public List<long> InputAsNumbers
         {
             get
             {
                 if (asNumbers.Count == 0)
                 {
                     Array.ForEach(Input, x => asNumbers.Add(Convert.ToInt64(x)));
-
                 }
                 return asNumbers;
             }
         }
-        public List<long> GetInputAsNumbers()
-        {
-            Array.ForEach(Input, x => AsNumbers.Add(Convert.ToInt64(x)));
-            return AsNumbers;
-        }
+
         public override string Level2(string[] input)
         {
+            InputAsNumbers.Add(0);
+            var n = InputAsNumbers.OrderBy(x => x).ToList();
+            n.Add(n[n.Count - 1] + 3);
 
-            AsNumbers.Add(0);
-            var n = AsNumbers.OrderBy(x => x).ToList();
-            var last = n[n.Count - 1] + 3;
-            n.Add(last);
-            Array.ForEach(n.ToArray(), Console.WriteLine);
-            var graph = new AdjacencyGraph<long, TaggedEdge<long,int>>();
-            Array.ForEach(n.ToArray(), x => graph.AddVertex(x));
-            Array.ForEach(n.ToArray(), x =>
-                Array.ForEach(n.Where(y => y - x <= 3 && y - x > 0).ToArray(), y => graph.AddEdge(createEdge(x, y))));
             var result = CountPathsToEnd(n, 0);
             return result.ToString();
         }
 
-		private TaggedEdge<long, int> createEdge(long x, long y)
-		{
-            return new TaggedEdge<long, int>(x, y, (int)(y-x));
-		}
         Dictionary<int, long> found = new();
-		private long CountPathsToEnd(List<long> list, int currentIndex)
+        private long CountPathsToEnd(List<long> list, int currentIndex)
         {
             if (list[currentIndex] == list[list.Count - 1]) return 1;
             var next = list.Where(x => x - list[currentIndex] <= 3 && x - list[currentIndex] > 0).ToList();
@@ -99,15 +101,15 @@ namespace AOC2020.Days
             {
                 var idx = list.IndexOf(item);
                 if (found.ContainsKey(idx))
-				{
+                {
                     paths += found[idx];
-				}else
-				{
-                    var p =  CountPathsToEnd(list, list.IndexOf(item));
+                }
+                else
+                {
+                    var p = CountPathsToEnd(list, list.IndexOf(item));
                     found.Add(idx, p);
                     paths += p;
-
-				}
+                }
             }
             return paths;
         }
---
+++ 2020/Days/Day11.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOC2020.Days
+{
+    public class Day11 : Christmas
+    {
+        public override int Day => 11;
+        public static void Run()
+        {
+            var day = new Day11();
+
+            Console.WriteLine($"hello day {day.Day}");
+            //day.GetInput(file: "test.txt", pattern: "\r\n");
+            day.GetInput();
+            Console.WriteLine(day.Level1(day.Input));
+            day.PostL1Answer();
+            //    Thread.Sleep(60000);
+            // Console.WriteLine(day.Level2(day.Input));
+            //day.PostL2Answer();
+        }
+        public int[,] Grid;
+        public override string Level1(string[] input)
+        {
+            Grid = new int[input.Length, input[0].Length];
+            for (int i = 0; i < input.Length; i++)
+            {
+                for (int j = 0; j < input[i].Length; j++)
+                {
+                    switch (input[i][j])
+                    {
+                        case 'L':
+                            Grid[i, j] = 0;
+                            break;
+                        case '.':
+                            Grid[i, j] = -1;
+                            break;
+                    }
+                }
+            }
+            while (Tick())
+            {
+                //	printGrid();
+            }
+            return CountOccupied(Grid).ToString();
+        }
+
+        private int CountOccupied(int[,] grid)
+        {
+            int ctr = 0;
+            (int, int) bounds = (grid.GetUpperBound(0) + 1, grid.GetUpperBound(1) + 1);
+            for (int i = 0; i < bounds.Item1; i++)
+            {
+                for (int j = 0; j < bounds.Item2; j++)
+                {
+                    if (grid[i, j] == 1) ctr++;
+                }
+            }
+            return ctr;
+        }
+
+        public void printGrid()
+        {
+
+            (int, int) bounds = (Grid.GetUpperBound(0) + 1, Grid.GetUpperBound(1) + 1);
+            for (int i = 0; i < bounds.Item1; i++)
+            {
+                for (int j = 0; j < bounds.Item2; j++)
+                    Console.Write(print(Grid[i, j]));
+                Console.WriteLine();
+            }
+        }
+
+        private char print(int v)
+        {
+            switch (v)
+            {
+                case 0: return 'L';
+                case -1: return '.';
+                case 1:
+                    return '#';
+                default: return 'Z';
+            }
+        }
+
+        public bool Tick()
+        {
+            int row = Grid.GetUpperBound(0) + 1;
+            int col = Grid.GetUpperBound(1) + 1;
+            int[,] nextGrid = new int[row, col];
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    nextGrid[i, j] = predict(Grid, i, j);
+                }
+            }
+            if (equivalent(Grid, nextGrid))
+                return false;
+            else
+                Grid = nextGrid;
+            return true;
+        }
+
+        private bool equivalent(int[,] grid, int[,] nextGrid)
+        {
+            (int, int) bounds = (grid.GetUpperBound(0) + 1, grid.GetUpperBound(1) + 1);
+            for (int i = 0; i < bounds.Item1; i++)
+            {
+                for (int j = 0; j < bounds.Item2; j++)
+                    if (grid[i, j] != nextGrid[i, j]) return false;
+            }
+            return true;
+        }
+
+        private int predict(int[,] grid, int i, int j)
+        {
+            int result = grid[i, j];
+            int occupied = CountAdjacentOccupied(grid, i, j);
+            if (occupied == 0)
+                return 1;
+            else if (occupied >= 4)
+                return 0;
+
+
+            return result;
+        }
+
+        private int CountAdjacentOccupied(int[,] grid, int i, int j)
+        {
+            int occupied = 0;
+            if (grid[i, j] == -1) return 1;
+            (int, int) bounds = (grid.GetUpperBound(0), grid.GetUpperBound(1));
+            for (int x = i - 1; x <= i + 1; x++)
+            {
+                if (x > bounds.Item1 || x < 0) continue;
+                for (int y = j - 1; y <= j + 1; y++)
+                {
+                    if (y > bounds.Item2 || y < 0 || (x == i && y == j)) continue;
+                    if (grid[x, y] == 1) occupied++;
+                }
+            }
+
+            return occupied;
+        }
+    }
+}
