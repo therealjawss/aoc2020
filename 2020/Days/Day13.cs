79d948e2b079a0512db2b001b9f922ae189297ca
jocelyn.englund <jocelyn.englund@coolcompany.se>
2020-12-13 14:23:35 +0100

    Day13: working but stack overflow on real input

--- 2020/Days/Day13.cs
+++ 2020/Days/Day13.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 
@@ -32,7 +33,48 @@ namespace AOC2020.Days
         }
         public override string Level2(string[] input)
         {
+            var r = Regex.Matches(input[1], @"(\d+|x)").Select(x => x.Groups[1].Value).ToList();
+            List<Bus> busses = new();
+            for (int i = 0; i < r.Count(); i++)
+            {
+                if (!r[i].Equals("x"))
+                {
+                    busses.Add(new Bus(i,Int64.Parse(r[i])));
+                }
+            }
+            long startTime = 0;
+            startTime = FindTime(busses, startTime);
+            Console.WriteLine(startTime);
             return base.Level2(input);
         }
+
+         long FindTime(Bus bus1, Bus bus2, long startTime)
+		{
+            startTime += bus1.number;
+            var mult = startTime / bus1.number;
+            if (bus2.offset == bus2.number - startTime % bus2.number) return startTime;
+            else return FindTime(bus1, bus2, startTime+bus1.number);
+		}
+        Dictionary<(long, long), long> mods = new();
+        long FindTime(List<Bus> busses, long starttime)
+        {
+            starttime += busses[0].number;
+
+            var b = busses.Where(b => b.offset > 0).ToList();
+            if (b.All(b => passes(starttime, b)))
+                return starttime;
+            else return FindTime(busses, starttime);
+		}
+       
+		private bool passes(long starttime, Bus b)
+		{ 
+            if (!mods.ContainsKey((starttime, b.number)))
+			{
+                mods.Add((starttime, b.number), starttime % b.number);
+			}
+            return b.offset == b.number - mods[(starttime, b.number)];
+		}
+
+		record Bus(long offset, long number);
     }
 }
