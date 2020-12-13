968d2b52d209e65b0dd89fab07ff13dfd5300de8
Jocelyn Englund <jocelyn.englund@coolcompany.se>
2020-12-13 23:01:11 +0100

    Prep for Day 14

---
+++ 2020/Days/Day14.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace AOC2020.Days
+{
+
+    public class Day14: Christmas
+    {
+        public override int Day => 14;
+
+        public static void Run()
+        {
+            var day =new Day14();
+            day.GetInput();
+            Console.WriteLine(day.Level1(day.Input));
+            Console.WriteLine(day.Level2(day.Input));
+            Console.ReadKey();
+        }
+
+		public override string Level1(string[] input)
+        {
+            var result ="";
+            return result;
+        }
+
+        public override string Level2(string[] input)
+        {
+            var result = "";
+
+            return result;
+        }
+
+    }
+}
