b8978fbd7751cd88d8cab02abb509d9e0ef038b6
jocelyn.englund <jocelyn.englund@coolcompany.se>
2020-12-28 16:17:06 +0100

    fixing merge issues

--- 2020/Days/Day23.cs
+++ 2020/Days/Day23.cs
@@ -59,65 +59,63 @@ namespace AOC2020.Days
 
 
 
-
-
-    }
-}
-// Console.WriteLine(d.Level1(d.Input));
-Console.WriteLine(d.Level2(d.Input));
-		}
-		int MAX = 1000000;
-public override string Level2(string[] input)
-{
-    var answer = "";
-    var cards = input[0].ToCharArray().Select(x => int.Parse(x.ToString())).Concat(MAX == 1000000 ? Enumerable.Range(10, 1000000 - 9) : new int[] { }).ToArray();
-    LinkedList<int> lcards = new LinkedList<int>(cards);
-    var current = lcards.First;
-    var last = lcards.Last;
-    Dictionary<int, LinkedListNode<int>> listindex = new Dictionary<int, LinkedListNode<int>>();
-    for (int i = 0; i < MAX; i++)
-    {
-        listindex.Add(current.Value, current);
-        current = current.Next;
-    }
-
-    current = lcards.First;
-    for (int i = 0; i < 10000000; i++)
-    {
-        LinkedListNode<int> first = current;
-        var one = first.GetAfterIn(lcards);
-        var two = one.GetAfterIn(lcards);
-        var three = two.GetAfterIn(lcards);
-        var next = three.GetAfterIn(lcards);
-
-        lcards.Remove(one);
-        lcards.Remove(two);
-        lcards.Remove(three);
-
-        var hand = new int[] { one.Value, two.Value, three.Value };
-        //var hand = cards[1..4];
-        var dest = first.Value - 1 == 0 ? MAX : first.Value - 1;
-
-        while (hand.Contains(dest))
+        int MAX = 1000000;
+        public override string Level2(string[] input)
         {
-            dest = dest - 1 == 0 ? MAX : dest - 1;
+            var answer = "";
+            var cards = input[0].ToCharArray().Select(x => int.Parse(x.ToString())).Concat(MAX == 1000000 ? Enumerable.Range(10, 1000000 - 9) : new int[] { }).ToArray();
+            LinkedList<int> lcards = new LinkedList<int>(cards);
+            var current = lcards.First;
+            var last = lcards.Last;
+            Dictionary<int, LinkedListNode<int>> listindex = new Dictionary<int, LinkedListNode<int>>();
+            for (int i = 0; i < MAX; i++)
+            {
+                listindex.Add(current.Value, current);
+                current = current.Next;
+            }
+
+            current = lcards.First;
+            for (int i = 0; i < 10000000; i++)
+            {
+                LinkedListNode<int> first = current;
+                var one = first.GetAfterIn(lcards);
+                var two = one.GetAfterIn(lcards);
+                var three = two.GetAfterIn(lcards);
+                var next = three.GetAfterIn(lcards);
+
+                lcards.Remove(one);
+                lcards.Remove(two);
+                lcards.Remove(three);
+
+                var hand = new int[] { one.Value, two.Value, three.Value };
+                //var hand = cards[1..4];
+                var dest = first.Value - 1 == 0 ? MAX : first.Value - 1;
+
+                while (hand.Contains(dest))
+                {
+                    dest = dest - 1 == 0 ? MAX : dest - 1;
+                }
+                var d = listindex[dest];
+                lcards.AddAfter(d, one);
+                lcards.AddAfter(one, two);
+                lcards.AddAfter(two, three);
+                current = current.GetAfterIn(lcards);
+
+            }
+            var o = listindex[1];
+            ulong ans = (ulong)o.Next.Value * (ulong)o.Next.Next.Value;
+            Console.WriteLine(ans);
+            answer = ans.ToString();
+            return answer.ToString();
+
         }
-        var d = listindex[dest];
-        lcards.AddAfter(d, one);
-        lcards.AddAfter(one, two);
-        lcards.AddAfter(two, three);
-        current = current.GetAfterIn(lcards);
+
+
 
     }
-    var o = listindex[1];
-    ulong ans = (ulong)o.Next.Value * (ulong)o.Next.Next.Value;
-    Console.WriteLine(ans);
-    answer = ans.ToString();
-    return answer.ToString();
-
 }
-	}
-	public static class ListExtensions
+
+public static class ListExtensions
 {
     public static int FindIndex(this int[] list, int number)
     {
@@ -133,5 +131,5 @@ public override string Level2(string[] input)
     }
 
 }
-}
+
 
