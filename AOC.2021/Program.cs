e40d4c40d2b533a14bc3782afb086d21d6592cab
Jocelyn Englund <jocelyn.englund@mirado.com>
2021-12-01 06:23:51 +0100

    2021-01-P2: brute part 2

--- AOC.2021/Program.cs
+++ AOC.2021/Program.cs
@@ -1,5 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Advent of Code 2021!\n**************");
 var input = await File.ReadAllTextAsync("input.txt");
 var lines = input.Split("\n").Select(x=>Int32.Parse(x)).ToArray();
 
@@ -9,6 +8,17 @@ foreach(var line in lines)
 {
     if (line > previous && previous !=-1) ctr++;
     previous = line;
+}
+
+Console.WriteLine(ctr);
+var previousWindow = -1;
+ctr= 0;
+for(int i = 1; i < lines.Length-1; i++)
+{
+    var current = lines[i - 1] + lines[i] + lines[i + 1];
+    if (previousWindow != -1 && current > previousWindow)
+        ctr++;
+    previousWindow = current;
 }
 
 Console.WriteLine(ctr);
