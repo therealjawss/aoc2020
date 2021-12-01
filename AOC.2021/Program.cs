8b0bbbb417c71b45019b2dec15fb492c893fee49
Jocelyn Englund <jocelyn.englund@mirado.com>
2021-12-01 06:12:35 +0100

    2021-01-P1: the bruteforce soltion

---
+++ AOC.2021/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+var input = await File.ReadAllTextAsync("input.txt");
+var lines = input.Split("\n").Select(x=>Int32.Parse(x)).ToArray();
+
+var ctr = 0;
+var previous = -1;
+foreach(var line in lines)
+{
+    if (line > previous && previous !=-1) ctr++;
+    previous = line;
+}
+
+Console.WriteLine(ctr);
