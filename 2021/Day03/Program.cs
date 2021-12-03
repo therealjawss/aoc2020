5dd04a666b6dbdf3e872e9fb9e178cb7c785839f
Jocelyn Englund <jocelyn.englund@mirado.com>
2021-12-03 06:32:05 +0100

    2021-12-03-P1

--- 2021/Day03/Program.cs
+++ 2021/Day03/Program.cs
@@ -4,6 +4,7 @@ var d = new Day3();
 await d.GetInput();
 
 Console.WriteLine($"Part 1:{d.First()}");
+
 Console.WriteLine($"Part 2:{d.Second()}");
 
 
@@ -15,12 +16,39 @@ public class Day3 : Christmas
 
     public override string First()
     {
-        return $"todo {1}";
+        var gamma = "";
+        var epsilon = "";
+        for (int i = 0; i < Input[0].Length; i++)
+        {
+            var toParse = Input.Select(x => x[i]);
+            if (toParse.Count(x => x == '0') > toParse.Count(x => x == '1')) {
+                gamma+="0";
+                epsilon+="1";
+            } else
+            {
+                gamma+="1";
+                epsilon+="0";
+            }
+        }
+        return (ToLong(gamma) * ToLong(epsilon)).ToString();
     }
 
     public override string Second()
     {
-        return $"todo {2}";
+       
     }
-   
+
+    public long ToLong(string binaryStr)
+    {
+        long result = 0;
+
+
+        for (int i = 0; i < binaryStr.Length; i++)
+        {
+            if (binaryStr[i] == '1')
+                result |= (1L << (binaryStr.Length - i -1));
+        }
+        return result;
+    }
+
 }
