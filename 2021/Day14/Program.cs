0602f46ce267242d861ab73757a32794ebcc311a
Jocelyn Englund <jocelyn.englund@hm.com>
2021-12-15 08:15:59 +0100

    2021-12-14: better late than never...

--- 2021/Day14/Program.cs
+++ 2021/Day14/Program.cs
@@ -1,8 +1,8 @@
 using ChristmasGifts;
 var d = new Day14();
-var newLine = "\r\n\r\n";
-await d.GetInput(file: "test.txt", pattern: newLine);
-//await d.GetInput(pattern: newLine); 
+var newLine = "\n\n";
+//await d.GetInput(file: "test.txt", pattern: newLine);
+await d.GetInput(pattern: newLine); 
 //await d.PostFirstAnswer(); 
 Console.WriteLine($"Part 1:{d.RunFirst()}");
 Console.WriteLine($"Part 2:{d.RunSecond()}");
@@ -52,26 +52,34 @@ public class Day14 : Christmas
         var irules = Input[1].Split("\n", StringSplitOptions.RemoveEmptyEntries);
         var rules = irules
             .Select(x => x.Split("->", StringSplitOptions.TrimEntries))
-            .ToDictionary(x => x[0], y => y[1]);
-        var polymer = template;
-        for (int k = 0; k< polymer.Length; k++)
+            .ToDictionary(x => x[0], y => y[1][0]);
+        var polymerCount = rules.ToDictionary(x => x.Key, y => 0UL);
+        var letterCount = polymerCount.Keys.SelectMany(x => x.ToCharArray()).Distinct().ToDictionary(x => x, y => 0UL);
+
+        for (int i = 0; i < template.Length - 1; i++)
         {
-            var subpolymer = polymer.Substring(k, 2);
-            for (int i = 0; i < 40; i++)
-            {
-                var newString = "" + subpolymer[0];
-                for (int j = 0; j < subpolymer.Length - 1; j++)
-                {
-                    var toAdd = rules[subpolymer.Substring(j, 2)];
-                    newString += toAdd + subpolymer[j + 1];
-
-                }
-                subpolymer = newString;
-            }
+            polymerCount[template.Substring(i, 2)] = 1;
         }
-        result = (polymer.ToCharArray().GroupBy(x => x).Max(x => x.Count()) -
-            polymer.ToCharArray().GroupBy(x => x).Min(x => x.Count())).ToString();
+        foreach(var c in template)
+        {
+            letterCount[c]++;
+        }
 
+        for (int i = 0; i < 40; i++)
+        {
+            var newPolymerCount = rules.ToDictionary(x => x.Key, y => 0UL);
+            foreach (var polymer in polymerCount.Where(x => x.Value > 0))
+            {
+                var count = polymer.Value;
+                var toAdd = rules[polymer.Key];
+                letterCount[toAdd] += count;
+                newPolymerCount[$"{polymer.Key[0]}{toAdd}"] += count;
+                newPolymerCount[$"{toAdd}{polymer.Key[1]}"] += count;
+            }
+            polymerCount = newPolymerCount;
+        }
+
+        result = (letterCount.Max(x => x.Value) - letterCount.Min(x => x.Value)).ToString();
         return result;
 
     }
