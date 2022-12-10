10af354a3d83cf7cce5f4475a5df7388b77cf573
Jocelyn Englund 📎 <jocelyn@itsybit.se>
2022-12-10 18:53:40 +0100

    Day10 Part 2. got so confused, it was better to start over.

--- 2022/Day10/Program.cs
+++ 2022/Day10/Program.cs
@@ -1,9 +1,9 @@
 using ChristmasGifts;
 var d = new Day10();
-//await d.GetInput(file: "test.txt", pattern: Environment.NewLine);
+//await d.GetInput(file: "test.txt");
 await d.GetInput();
 Console.WriteLine($"Part 1:{d.RunFirst()}");
-await d.PostFirstAnswer();
+//await d.PostFirstAnswer();
 Console.WriteLine($"Part 2:{d.RunSecond()}");
 //await Task.Delay(5000); 
 //await d.PostSecondAnswer(); 
@@ -15,7 +15,6 @@ public class Day10 : Christmas
     {
         int cycleToWatch = 20;
         int currentCycle = 1;
- 
         long totalSignalStrength = 0;
         int cycleAge = Input[0].Trim().Split(' ').Length; ;
 
@@ -40,10 +39,34 @@ public class Day10 : Christmas
         return result;
     }
 
-    public record Instruction(int increment);
 
     public override string Second()
     {
+        var total = Input.Count(x => x.StartsWith("noop")) + Input.Count(x => x.StartsWith("add")) * 2;
+        int ptr = 0;
+        int spritelocation = 1;
+
+        int currentCycle = 1;
+        int cycleAge = Input[ptr].Trim().Split(' ').Length;
+        for (int position = 0; ptr<Input.Length; position++, currentCycle++)
+        {
+            cycleAge--;
+            var charToPrint = position%40 >= spritelocation-1 && position%40<= spritelocation+1 ? '#' : '.';
+            Console.Write(charToPrint);
+            if (position %40==39) Console.WriteLine();
+            if (cycleAge==0)
+            {
+                ptr++;
+                if (ptr<Input.Length)
+                {
+                    spritelocation =Input[..(ptr)].Select(x => x.Trim().Split(' ')).Where(x => x.Length ==2).Sum(x => int.Parse(x[1]))+1;
+                    cycleAge = Input[ptr].Trim().Split(' ').Length;
+                }
+               
+            }
+        }
+        result = "\n";
         return result;
+
     }
 }
